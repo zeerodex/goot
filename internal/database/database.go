a65fe3363f78bcbe06958ece27fb8c03e5751ff1
zeerodex <sdemianenko25@gmail.com>
2025-04-28 12:45:54 +0300

    db init

---
+++ internal/database/database.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"database/sql"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func InitDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "database.db")
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	stmt := `
+	CREATE TABLE IF NOT EXISTS tasks (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	task TEXT,
+	description TEXT,
+	status INTEGER)`
+
+	_, err = db.Exec(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
