5d1cd7453c707990ecac197fa730da0d37a5b601
zeerodex <sdemianenko25@gmail.com>
2025-04-28 12:48:56 +0300

    main.go, base setup complete

---
+++ cmd/go-todo-tui/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/zeerodex/go-todo-tui/internal/database"
+	"github.com/zeerodex/go-todo-tui/internal/tasks"
+	"github.com/zeerodex/go-todo-tui/internal/tui"
+)
+
+func main() {
+	db, err := database.InitDB()
+	if err != nil {
+		fmt.Println("error init db:", err)
+		return
+	}
+	defer db.Close()
+
+	taskService := tasks.NewTaskService(tasks.NewTaskRepository(db))
+
+	if _, err := tea.NewProgram(tui.InitAppModel(taskService)).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
+}
