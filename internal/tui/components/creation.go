b737da978e669f4098ab86e289728bcb8c8fe027
zeerodex <sdemianenko25@gmail.com>
2025-05-11 22:29:21 +0300

    working multiple text inputs

--- internal/tui/components/creation.go
+++ internal/tui/components/creation.go
@@ -2,19 +2,31 @@ package components
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/zeerodex/go-todo-tui/pkg/timeutil"
 )
 
-type CreationModel struct {
-	TaskInput textinput.Model
-	DescInput textinput.Model
-	DueInput  textinput.Model
+var (
+	focusedStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+	blurredStyle        = lipgloss.NewStyle().Foreground(lipgloss.Color("240"))
+	cursorStyle         = focusedStyle
+	noStyle             = lipgloss.NewStyle()
+	helpStyle           = blurredStyle
+	cursorModeHelpStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("244"))
 
-	Step   int
+	focusedButton = focusedStyle.Render("[ Submit ]")
+	blurredButton = fmt.Sprintf("[ %s ]", blurredStyle.Render("Submit"))
+)
+
+type CreationModel struct {
+	focusIndex int
+	inputs     []textinput.Model
+
 	Done   bool
 	Result Task
 }
@@ -26,28 +38,32 @@ type Task struct {
 }
 
 func InitialCreationModel() CreationModel {
-	taskInput := textinput.New()
-	taskInput.Focus()
-	taskInput.Placeholder = "Title (Required)"
-	taskInput.CharLimit = 156
-	taskInput.Width = 20
-
-	descInput := textinput.New()
-	descInput.Placeholder = "Description (Not required)"
-	descInput.CharLimit = 156
-	descInput.Width = 40
-
-	dueInput := textinput.New()
-	dueInput.Placeholder = "Due [YYYY-MM-DD (HH:MM)] (Today by default)"
-	dueInput.CharLimit = 156
-	dueInput.Width = 40
-
-	return CreationModel{
-		TaskInput: taskInput,
-		DescInput: descInput,
-		DueInput:  dueInput,
-		Step:      1,
+	m := CreationModel{
+		inputs: make([]textinput.Model, 3),
 	}
+	var t textinput.Model
+	for i := range m.inputs {
+		t = textinput.New()
+		switch i {
+		case 0:
+			t.Focus()
+			t.Placeholder = "Title (Required)"
+			t.CharLimit = 156
+			t.Width = 50
+		case 1:
+			t.Placeholder = "Description (Not required)"
+			t.CharLimit = 156
+			t.Width = 50
+		case 2:
+			t.Placeholder = "Due [YYYY-MM-DD (HH:MM)] (Today by default)"
+			t.CharLimit = 156
+			t.Width = 50
+
+		}
+		m.inputs[i] = t
+	}
+
+	return m
 }
 
 func (m CreationModel) Init() tea.Cmd {
@@ -56,87 +72,92 @@ func (m CreationModel) Init() tea.Cmd {
 
 func (m CreationModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
-	var cmds []tea.Cmd
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "esc":
 			return m, tea.Quit
-		case "enter":
-			switch m.Step {
-			case 1:
-				if m.TaskInput.Value() == "" {
-					m.TaskInput.Placeholder = "Task title cannot be empty"
-				} else {
-					m.TaskInput.Blur()
-					m.DescInput.Focus()
+		case "tab", "shift+tab", "enter", "up", "down":
+			s := msg.String()
 
-					m.Step = 2
-					return m, nil
-				}
-			case 2:
-				m.DescInput.Blur()
-				m.DueInput.Focus()
-
-				m.Step = 3
-				return m, nil
-			case 3:
-				due, err := timeutil.ParseAndValidateTimestamp(m.DueInput.Value())
-				if err != nil {
-					m.DueInput.SetValue("")
-					m.DueInput.Placeholder = err.Error()
+			if s == "enter" && m.focusIndex == len(m.inputs) {
+				due, err := timeutil.ParseAndValidateTimestamp(m.inputs[2].Value())
+				if m.inputs[0].Value() == "" {
+					m.inputs[0].Placeholder = "Task title cannot be empty"
+				} else if err != nil {
+					m.inputs[2].SetValue("")
+					m.inputs[2].Placeholder = err.Error()
 				} else {
 					m.Done = true
 
 					m.Result = Task{
-						Title:       m.TaskInput.Value(),
-						Description: m.DescInput.Value(),
+						Title:       m.inputs[0].Value(),
+						Description: m.inputs[1].Value(),
 						Due:         due,
 					}
-
+					return m, nil
 				}
 			}
+
+			if s == "up" || s == "shift+tab" {
+				m.focusIndex--
+			} else {
+				m.focusIndex++
+			}
+
+			if m.focusIndex > len(m.inputs) {
+				m.focusIndex = 0
+			} else if m.focusIndex < 0 {
+				m.focusIndex = len(m.inputs)
+			}
+
+			cmds := make([]tea.Cmd, len(m.inputs))
+			for i := range m.inputs {
+				if i == m.focusIndex {
+					cmds[i] = m.inputs[i].Focus()
+					m.inputs[i].PromptStyle = focusedStyle
+					m.inputs[i].TextStyle = focusedStyle
+					continue
+				}
+				m.inputs[i].Blur()
+				m.inputs[i].PromptStyle = noStyle
+				m.inputs[i].TextStyle = noStyle
+			}
+
+			return m, tea.Batch(cmds...)
 		}
 	}
+	cmd = m.updateInputs(msg)
 
-	switch m.Step {
-	case 1:
-		m.TaskInput, cmd = m.TaskInput.Update(msg)
-		cmds = append(cmds, cmd)
-	case 2:
-		m.DescInput, cmd = m.DescInput.Update(msg)
-		cmds = append(cmds, cmd)
-	case 3:
-		m.DueInput, cmd = m.DueInput.Update(msg)
-		cmds = append(cmds, cmd)
+	return m, cmd
+}
+
+func (m *CreationModel) updateInputs(msg tea.Msg) tea.Cmd {
+	cmds := make([]tea.Cmd, len(m.inputs))
+
+	for i := range m.inputs {
+		m.inputs[i], cmds[i] = m.inputs[i].Update(msg)
 	}
 
-	return m, tea.Batch(cmds...)
+	return tea.Batch(cmds...)
 }
 
 func (m CreationModel) View() string {
-	var s string
-	if m.Step == 1 {
-		s = fmt.Sprintf(
-			"What's your task?\n\n%s",
-			m.TaskInput.View(),
-		) + "\n"
+	var b strings.Builder
+
+	for i := range m.inputs {
+		b.WriteString(m.inputs[i].View())
+		if i < len(m.inputs)-1 {
+			b.WriteRune('\n')
+		}
 	}
 
-	if m.Step == 2 {
-		s += fmt.Sprintf(
-			"Enter a description:\n\n%s",
-			m.DescInput.View()) + "\n\n"
-		s += "Press Enter to save"
+	button := &blurredButton
+	if m.focusIndex == len(m.inputs) {
+		button = &focusedButton
 	}
+	fmt.Fprintf(&b, "\n\n%s\n\n", *button)
 
-	if m.Step == 3 {
-		s += fmt.Sprintf(
-			"Enter a due date:\n\n%s",
-			m.DueInput.View()) + "\n\n"
-		s += "Press Enter to save"
-	}
-
-	return s
+	return b.String()
 }
