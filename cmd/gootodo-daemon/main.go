66f3244edc42b05f905ec0865a72716c0b00a381
zeerodex <sdemianenko25@gmail.com>
2025-05-13 22:04:55 +0300

    in progress

--- cmd/gootodo-daemon/main.go
+++ cmd/gootodo-daemon/main.go
@@ -1,27 +1,57 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
+	"os/signal"
 	"time"
 
 	"github.com/zeerodex/go-todo-tui/internal/database"
 	"github.com/zeerodex/go-todo-tui/internal/tasks"
 )
 
-func CheckTasks(repo tasks.TaskRepository) error {
+type TaskProcessor struct {
+	db           *sql.DB
+	repo         tasks.TaskRepository
+	batchSize    int
+	timeWindow   time.Duration
+	pollInterval time.Duration
+}
+
+func NewTaskProcessor(db *sql.DB, repo tasks.TaskRepository, batchSize int, timeWindow, pollInterval time.Duration) *TaskProcessor {
+	return &TaskProcessor{db: db, repo: repo, batchSize: batchSize, timeWindow: timeWindow, pollInterval: pollInterval}
+}
+
+func (tp *TaskProcessor) Start(ctx context.Context) {
+	ticker := time.NewTicker(tp.pollInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+		case <-ctx.Done():
+			log.Println("Daemon shutting down")
+			return
+		}
+	}
+}
+
+func (tp *TaskProcessor) CheckTasks(repo tasks.TaskRepository) error {
 	now := time.Now()
 	due := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), 0, 0, now.Location())
 	task, err := repo.GetByDue(due)
 	if err != nil {
 		return fmt.Errorf("failed to get task: %w", err)
 	}
-	go SendTaskNofitication(task)
+	go SendTaskDueNofitication(task)
 	return nil
 }
 
-func SendTaskNofitication(task tasks.Task) error {
+func SendTaskDueNofitication(task tasks.Task) error {
 	icon := "task-due-symbolic"
 	cmd := exec.Command("notify-send", task.Title, task.Description, "-i", icon)
 	err := cmd.Run()
@@ -38,19 +68,23 @@ func main() {
 	}
 	defer db.Close()
 
-	repo := tasks.NewTaskRepository(db)
+	tp := NewTaskProcessor(db, tasks.NewTaskRepository(db), 5, time.Minute, 5*time.Second)
 
 	ticker := time.NewTicker(1 * time.Second)
 	defer ticker.Stop()
 
-	for {
-		select {
-		case <-ticker.C:
-			log.Println("Check")
-			err = CheckTasks(repo)
-			if err != nil {
-				fmt.Println(err)
-			}
-		}
-	}
+	sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, os.Interrupt)
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	go tp.Start(ctx)
+
+	<-sigs
+	log.Println("Interrupt received, shutting down...")
+	cancel()
+
+	log.Println("Daemon shutdown completed")
 }
